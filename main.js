25e3fdc3bc42015cf64ce071b535af035ccac238
Eliaswalle <wael22nq@student.ju.se>
2022-12-05 08:40:43 +0100

    POP OUT

--- main.js
+++ main.js
@@ -1,5 +1,9 @@
 const hamburger = document.querySelector(".hamburger");
+const mobile_menu = document.querySelector(".mobile-nav");
 
 hamburger.addEventListener("click", function () {
   this.classList.toggle("is-active");
+});
+mobile_menu.addEventListener("click", function () {
+  mobile_menu.classList.toggle("is-active");
 });
