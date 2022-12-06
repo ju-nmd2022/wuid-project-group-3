2a3ef4ff54433476436417abc1ccc501cdab88d7
Eliaswalle <wael22nq@student.ju.se>
2022-12-06 15:24:30 +0100

    nav bar for mobile

--- main.js
+++ main.js
@@ -1,9 +1,8 @@
 const hamburger = document.querySelector(".hamburger");
-const mobile_menu = document.querySelector(".mobile-nav");
+const navMenu = document.querySelector(".nav-menu");
 
-hamburger.addEventListener("click", function () {
-  this.classList.toggle("is-active");
-});
-mobile_menu.addEventListener("click", function () {
-  mobile_menu.classList.toggle("is-active");
-});
+hamburger.addEventListener("click", () => {
+hamburger.classList.toggle("is-active");
+navMenu.classList.toggle("active");
+})
+
