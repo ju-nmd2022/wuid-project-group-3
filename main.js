fdc7b9cd636ccaae409a745f66558ba33921a260
Eliaswalle <wael22nq@student.ju.se>
2022-12-06 18:53:56 +0100

    final page for me changes

--- main.js
+++ main.js
@@ -2,7 +2,16 @@ const hamburger = document.querySelector(".hamburger");
 const navMenu = document.querySelector(".nav-menu");
 
 hamburger.addEventListener("click", () => {
-hamburger.classList.toggle("is-active");
-navMenu.classList.toggle("active");
-})
+  hamburger.classList.toggle("is-active");
+  navMenu.classList.toggle("active");
+});
 
+const dropdowns = document.querySelectorAll(".dropdown");
+
+dropdowns.forEach((dropdown) => {
+  const select = dropdown.querySelector(".select");
+  const caret = dropdown.querySelector(".caret");
+  const menu = dropdown.querySelector(".dropdown-menu");
+  const options = dropdown.querySelectorAll(".dropdown-menu li");
+  const selected = dropdown.querySelector(".selected");
+});
