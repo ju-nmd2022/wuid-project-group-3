2b4c2b3e77cd6474ec7999ad0d166749a4717807
Eliaswalle <wael22nq@student.ju.se>
2022-12-07 18:25:36 +0100

    final js page. plus pictures

---
+++ final-page.js
@@ -0,0 +1,20 @@
+const hamburger = document.querySelector(".hamburger");
+const navMenu = document.querySelector(".nav-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger.classList.toggle("is-active");
+  navMenu.classList.toggle("active");
+});
+let bnt = document.getElementById("bnt");
+let balert = document.getElementById("bnt-alert");
+let X = document.getElementById("X");
+bnt.onclick = function () {
+  balert.style.display = "block";
+};
+X.onclick = function () {
+  balert.style.display = "none";
+};
+let back = document.getElementById("back");
+back.onclick = function () {
+  window.location.replace("index.html");
+};
